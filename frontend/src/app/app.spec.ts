dd51447d5f58f46113c3b836ccc97732f9126c98
Smiley-Isra-chahrazed <goumiri.isra@gmail.com>
2026-03-27 22:12:06 +0100

    ✅ test: add tests for the implemented features

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { ActivatedRoute } from '@angular/router';
+import { Counter } from './services/counter';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+            { provide: ActivatedRoute, useValue: { snapshot: { params: {} } },
+          Counter }
+          ]
     }).compileComponents();
   });
 
@@ -18,6 +24,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('CounterNest');
   });
 });
--- frontend/src/app/services/counter.spec.ts
+++ frontend/src/app/services/counter.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { Counter } from './counter';
+import { ActivatedRoute } from '@angular/router';
 
 describe('Counter', () => {
   let service: Counter;
@@ -12,5 +12,48 @@ describe('Counter', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // ✅ Increment
+  it('should increment the counter by step', () => {
+    service.increment();
+    expect(service.counter).toBe(1);
+  });
+
+  // ✅ Decrement
+  it('should decrement the counter by step', () => {
+    service.decrement();
+    expect(service.counter).toBe(-1);
+  });
+
+  // ✅ Step doubling every 30 actions
+  it('should double steps after 30 actions', () => {
+    for (let i = 0; i < 30; i++) {
+      service.increment();
+    }
+
+    expect(service.steps).toBe(2);
+  });
+
+  // ✅ Step doubling multiple times
+  it('should keep doubling steps every 30 actions', () => {
+    for (let i = 0; i < 60; i++) {
+      service.increment();
+    }
+
+    expect(service.steps).toBe(4); // 1 → 2 → 4
+  });
+
+
+  // ✅ Reset
+  it('should reset counter, actionsNumber and step', () => {
+    service.increment();
+    service.increment();
+
+    service.reset();
+
+    expect(service.counter).toBe(0);
+    expect(service.actionsNumber).toBe(0);
+    expect(service.steps).toBe(1);
   });
 });
