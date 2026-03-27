c54805e5b91b7efbbf9a16a18048dff8a7372974
Smiley-Isra-chahrazed <goumiri.isra@gmail.com>
2026-03-27 22:11:22 +0100

    ✨ feat: implement double step feature based on actions number

--- frontend/src/app/services/counter.ts
+++ frontend/src/app/services/counter.ts
@@ -5,18 +5,33 @@ import { Injectable } from '@angular/core';
 })
 export class Counter {
   counter = 0;
-
+  actionsNumber = 0;
+  steps = 1;
 
   increment() {
-    this.counter += 1;
+    this.actionsNumber++;
+    this.updateStep();
+    this.counter += this.steps;
   }
 
   decrement() {
-    this.counter -= 1;
+    this.actionsNumber++;
+    this.updateStep();
+    this.counter -= this.steps;
   }
- reset() {
-  this.counter= 0;
- }
+
+  reset() {
+    this.counter = 0;
+    this.actionsNumber = 0;
+    this.steps = 1;
+  }
+
+  updateStep() {
+    if (this.actionsNumber % 30 === 0) {
+      this.steps *= 2;
+    }
+  }
+
 
 
 
