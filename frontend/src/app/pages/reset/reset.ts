b0b5fbc42b227f966d31652690207acc4b84bcb3
Smiley-Isra-chahrazed <goumiri.isra@gmail.com>
2026-03-28 23:27:32 +0100

    💄 update: update UI

--- frontend/src/app/pages/reset/reset.ts
+++ frontend/src/app/pages/reset/reset.ts
@@ -3,11 +3,12 @@ import { CounterService } from '../../services/counter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-reset',
   imports: [    MatButtonModule,
-    MatInputModule,    FormsModule,
+    MatInputModule,    FormsModule,MatDatepickerModule
 ],
   templateUrl: './reset.html',
   styleUrl: './reset.css',
